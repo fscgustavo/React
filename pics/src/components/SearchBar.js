9175b11fda7664a40649c1d571cba913c5dadfd9
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-02-12 16:09:46 -0300

    Controlled Element

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,11 +1,40 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    
+    //this name is a community convention 
+    //or handleInputChange
+    onInputChange(event){
+
+    }
+
+    state={ term: '' }
+
+    /* 
+                        
+        We do not put on a set of parentheses when ever we pass a callback function
+        to an event handler 
+
+        Another way to write this eventHandler
+
+        <input type="text" onChange={(e)=> console.log(e.target.value)}/>
+
+    */
+
     render(){
         return(
-            <div>
-                <form>
-                    <input type="text"/>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        
+                        <input 
+                            type="text" 
+                            value={this.state.term}
+                            onChange={e => this.setState({term: e.target.value})}
+                        />
+                        
+                    </div>
                 </form>
             </div>
         );
