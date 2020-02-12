d9bd9568e2e8c21a552fb846e7f504e46bbbcfdd
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-02-12 12:10:28 -0300

    Default prop e alteração de render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -35,7 +35,7 @@ class App extends React.Component{
             );
         }
 
-        return  <Spinner/>;
+        return  <Spinner message="Please accept location request"/>;
         
         
     }
