cbf335d2135d3ac88d315d7e7cfc6a68955cbe00
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-02-18 17:29:20 -0300

    Thunk e novo reducer

--- aulasRedux/blog/src/actions/index.js
+++ aulasRedux/blog/src/actions/index.js
@@ -1,12 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-//with async await: error: does not return a plain object
+//with async await, you have dispatch
 export const fetchPosts = () =>{
+    return async dispatch => {
+        const response = await jsonPlaceholder.get('/posts')
 
-    const promise = jsonPlaceholder.get('/posts')
-
-    return{
-        type: 'FETCH_POSTS',
-        payload: promise
+        dispatch({
+            type: 'FETCH_POSTS', payload: response
+        })
     }
 }
+
