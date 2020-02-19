a2ec45d28f0233e9e31f1186bdbe3f0ca9f521bd
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-02-19 17:54:59 -0300

    Adição de Streams + Conhecimento sobre React Router

--- aulasRedux/stream/client/src/components/App.js
+++ aulasRedux/stream/client/src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { BrowserRouter, Route, Link} from 'react-router-dom'
+import StreamList from './streams/StreamList'
+import StreamDelete from './streams/StreamDelete'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamShow from './streams/StreamShow'
+
 
 const App = () => {
     return(
-        <div>
-            App
-        </div>
+        <h1></h1>
+        <BrowserRouter>
+            <div>
+                <Route path="/" exact component={StreamList}/>
+                <Route path="/streams/new" exact component={StreamCreate}/>
+                <Route path="/streams/:id" exact component={StreamShow}/>
+                <Route path="/streams/edit/:id" exact component={StreamEdit}/>
+                <Route path="/streams/delete/:id" exact component={StreamDelete}/>
+            </div>
+        </BrowserRouter>
     );
 }
 
