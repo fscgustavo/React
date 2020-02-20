6219edcb7a50a7352508b52bef2cda63fa7ac6d4
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-02-20 18:30:34 -0300

    Customização dos FIELDS

--- aulasRedux/stream/client/src/components/streams/StreamCreate.js
+++ aulasRedux/stream/client/src/components/streams/StreamCreate.js
@@ -1,11 +1,54 @@
 import React from 'react'
+//Field is a component
+//reduxForm is a function
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () =>{
-    return(
-        <div>
-            StreamCreate
-        </div>
-    )
+class StreamCreate extends React.Component{
+    
+    renderInput({input, label}){//destructed formProps
+        
+        return(
+            <div class="field">
+                <label>{label}</label>
+                <input 
+                    {...input}
+                />
+            </div>
+        )
+
+    }
+    
+    
+    render(){
+        return(
+            <form className="ui form">
+                <Field 
+                    name="title" 
+                    component={this.renderInput}
+                    label = "Enter Title"
+                />
+                <Field 
+                    name="description" 
+                    component={this.renderInput}
+                    label="Enter Description"
+                    
+                />
+            </form>
+        )
+    }
+    
 }
 
-export default StreamCreate
+//new syntax
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate)
+
+
+/*
+long syntax of the input
+
+onChange={formProps.input.onChange}
+value={formProps.input.value}
+
+*/
