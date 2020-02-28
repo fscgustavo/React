c5f3ff82b28520b765037d7c19c644c7e8067692
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-02-28 15:43:25 -0300

    Projeto seasons com Hooks

---
+++ aulasRedux/seasons-hooks/src/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+
+class App extends React.Component{
+
+    /*constructor(props){
+        super(props);  
+    }*/
+
+
+    state = {
+        lat: null,
+        errorMessage:''
+    };
+
+    
+
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return(
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>
+            );
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return(
+                //um state é passado da mesma maneira que
+                // um prop
+                <SeasonDisplay
+                    lat={this.state.lat}
+                />
+            );
+        }
+
+        return  <Spinner message="Please accept location request"/>;     
+    }
+
+    //evite ter múltiplos returns no render
+    render(){
+        return(
+            <div className = "border red">
+                {this.renderContent()}
+            </div>
+        );
+    }
+
+    //Content visible on the screen
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            //we called setState!!!!!!!!
+            position => this.setState({lat: position.coords.latitude}),
+            //NOT: this.state.lat = position.coords.latitude
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    //sit and wait for updates
+    componentDidUpdate(){
+        console.log('My component was just updated - it rerendered!')
+    }
+    componentWillUnmount(){
+        
+    }
+
+}
+
+ReactDOM.render(
+    <App  />,
+    document.querySelector(`#root`)
+)
