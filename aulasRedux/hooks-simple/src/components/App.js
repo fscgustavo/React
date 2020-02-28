26da31bc95e0fd0b1672dd5cd0bd5cae06ba1497
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-02-28 12:28:59 -0300

    Projeto Hooks-Simple

---
+++ aulasRedux/hooks-simple/src/components/App.js
@@ -0,0 +1,16 @@
+  
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
+
+const App = () => {
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default App;
