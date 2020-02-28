a10c63917afc30f3812fcaf329b4ba7315cb9fae
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-02-28 16:21:08 -0300

    Fim Projeto Season-Hooks

--- aulasRedux/seasons-hooks/src/index.js
+++ aulasRedux/seasons-hooks/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 import useLocation from './useLocation'
@@ -25,5 +26,9 @@ const App = () => {
 
     return <div className="border red">{content}</div>
 }
+
+ReactDOM.render(
+    <App/>, document.querySelector('#root')
+)
 
 export default App
